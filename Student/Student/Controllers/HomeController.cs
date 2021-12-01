66261c8d8b76007cb54bd7f6cd04a5186763ccec
MajidovJavid <68659371+MajidovJavid@users.noreply.github.com>
2021-12-01 17:29:42 +0400

    Add files via upload

---
+++ Student/Student/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
---
+++ Student/Student/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return Content("Id is null");
+            }
+
+            return Json(new { Name = "Barak", Surname = "Obama", Age = 23 });
+        }
+
+
+    }
+}
